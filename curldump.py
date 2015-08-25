dfeca013869a50e0df0c499734b6d7047ae6a64d
Denis Fondras <github@ggl.ledeuns.net>
2015-08-25 15:55:20 +0200

    Response requires a 'R'

--- curldump.py
+++ curldump.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file, response, render_template, make_response
+from flask import Flask, request, send_file, Response, render_template, make_response
 import datetime
 import hashlib
 import os
